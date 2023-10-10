5e6e9185f3fa623d9032746acaece108920b91e6
rezuan <rezuan.ahmed.pro@gmail.com>
2023-10-10 17:20:31 +0600

    fix app routes

--- src/appRoutes.js
+++ src/appRoutes.js
@@ -2,9 +2,14 @@ const router = require('express').Router();
 const authenticate = require('./authenticateMiddleware');
 const authorize = require('./authorizeMiddleware');
 
-router.get('/public', (req, res) => {
-  res.status(200).json({ message: 'I am public' });
-});
+router.get(
+  '/public',
+  authenticate,
+  authorize(['admin', 'user']),
+  (req, res) => {
+    res.status(200).json({ message: 'I am public' });
+  }
+);
 
 router.get('/private', authenticate, authorize(['admin']), (req, res) => {
   res.status(200).json({ message: 'I am PRIVATE' });
