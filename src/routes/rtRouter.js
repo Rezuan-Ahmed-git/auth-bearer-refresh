09b2b11fc2dfe5b299721df1449ac68e9a423a08
rezuan <rezuan.ahmed.pro@gmail.com>
2023-10-11 08:25:07 +0600

    feat: refresh token rotating implement

--- src/routes/rtRouter.js
+++ src/routes/rtRouter.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const jwt = require('jsonwebtoken');
+const { addDays } = require('date-fns');
 const RefreshToken = require('../models/RefreshTokenModel');
 
 router.post('/refresh', async (req, res) => {
@@ -7,11 +8,38 @@ router.post('/refresh', async (req, res) => {
 
   try {
     const decode = jwt.verify(token, 'MY_JWT');
-    const refreshToken = await RefreshToken.findById(decode._id);
+    const oldRefreshToken = await RefreshToken.findById(decode._id);
 
-    if (!refreshToken || !refreshToken.isActive) {
+    if (!oldRefreshToken || !oldRefreshToken.isActive) {
       return res.status(400).json({ message: 'Invalid Token' });
     }
+
+    oldRefreshToken.revokedAt = new Date();
+    oldRefreshToken.revokedIp = req.clientIp;
+
+    await oldRefreshToken.save();
+
+    //generate new refresh token
+    //database object
+    const refreshToken = new RefreshToken({
+      user: decode.user,
+      issuedIp: req.clientIp || 'N/A',
+      token: '',
+      expiredAt: addDays(new Date(), 30),
+    });
+
+    const rToken = jwt.sign(
+      {
+        _id: refreshToken._id,
+        user: decode.user,
+        name: decode.name,
+        email: decode.email,
+      },
+      'MY_JWT'
+    );
+
+    refreshToken.token = rToken;
+    await refreshToken.save();
 
     const accessToken = jwt.sign(
       { _id: decode.user, name: decode.name, email: decode.email },
@@ -19,8 +47,9 @@ router.post('/refresh', async (req, res) => {
       { expiresIn: '30s' }
     );
 
-    res.status(200).json({ accessToken });
+    res.status(200).json({ accessToken, refreshToken: rToken });
   } catch (e) {
+    console.log(e);
     res.status(400).json({ message: 'Invalid Token' });
   }
 });
